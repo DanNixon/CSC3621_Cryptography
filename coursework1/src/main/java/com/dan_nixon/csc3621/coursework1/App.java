e462f9fcf9e1bdbec2d621952fb33147304baa72
Dan Nixon <dan@dan-nixon.com>
2015-10-13 19:29:25 +0100

    INitial Maven project and docs

---
+++ coursework1/src/main/java/com/dan_nixon/csc3621/coursework1/App.java
@@ -0,0 +1,13 @@
+package com.dan_nixon.csc3621.coursework1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
