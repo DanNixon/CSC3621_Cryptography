a3279669169976d1969d64ed0539af9539957063
Dan Nixon <dan@dan-nixon.com>
2015-10-27 17:26:15 +0000

    Analysis class done
    [ci skip]

--- coursework1_implementation/src/main/java/com/dan_nixon/csc3621/cw1/ex3/OTPAttack.java
+++ coursework1_implementation/src/main/java/com/dan_nixon/csc3621/cw1/ex3/OTPAttack.java
@@ -1,13 +1,142 @@
 package com.dan_nixon.csc3621.cw1.ex3;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import com.dan_nixon.csc3621.cw1.Utils;
+import com.dan_nixon.csc3621.cw1.ex1.FrequencyCounter;
+
 public class OTPAttack
 {
+  /**
+   * Loads cipher files to a 2D array of bytes.
+   *
+   * @param files Files to load
+   * @return Array of byte arrays
+   */
+  public static byte[][] loadCiphersFromFiles(File[] files) throws IOException
+  {
+    byte[][] ciphers = new byte[files.length][];
+    for (int i = 0; i < files.length; i++)
+      ciphers[i] = Utils.readBinaryFile(files[i]);
+    return ciphers;
+  }
+
+  /**
+   * Initialise a new OTPAttack.
+   *
+   * @param ciphers Array of byte arrays containing test ciphers
+   */
   public OTPAttack(byte[][] ciphers)
   {
     m_ciphers = ciphers;
   }
 
-  //TODO
+  /**
+   * Initialise a new OTPAttack.
+   *
+   * @param cipherFiles Array of cipher files to load
+   */
+  public OTPAttack(File[] cipherFiles) throws IOException
+  {
+    m_ciphers = loadCiphersFromFiles(cipherFiles);
+  }
+
+  /**
+   * Gets an ASCII representation of the target cipher data XORed with each of
+   * the analysis cipher data.
+   *
+   * This data can be used for manual inspection and automated plain text
+   * recovery.
+   *
+   * @param cipher Cipher data to cryptanalyse
+   * @return Array of analysis strings
+   */
+  public String[] getAnalysisStrings(byte[] cipher)
+  {
+    String[] analysisStrings = new String[m_ciphers.length];
+    int n = 0;
+
+    for (int i = 0 ; i < m_ciphers.length; i++)
+    {
+      if (Arrays.equals(cipher, m_ciphers[i]))
+        continue;
+
+      byte[] xor = Utils.arrayXor(cipher, m_ciphers[i]);
+      analysisStrings[n] = Utils.byteArrayToString(xor);
+      n++;
+    }
+
+    String[] result = new String[n];
+    System.arraycopy(analysisStrings, 0, result, 0, n);
+    return result;
+  }
+
+  /**
+   * Retrieve as much of the plain text message as possible given the data
+   * obtained through analysis of the XOR of the target cipher with analysis
+   * ciphers.
+   *
+   * Unknown characters are represented by a '-'.
+   *
+   * @param analysisStrings Cryptanalysis strings
+   * @param cipherLength Length of the original cipher data
+   * @param validCharThreshold Threshold at which a characters is assumed to be
+   *                           correct in a given position
+   * @return Best guess of plain text message
+   */
+  public String guessPlainTextFromAnalysis(String[] analysisStrings,
+                                           int cipherLength,
+                                           double validCharThreshold)
+  {
+    char[] messageChars = new char[cipherLength];
+
+    // Iterate over characters in the cipher message
+    for (int i = 0; i < cipherLength; i++)
+    {
+      StringBuilder sb = new StringBuilder();
+
+      // Iterate over analysis strings
+      for (int j = 0; j < analysisStrings.length; j++)
+      {
+        if (analysisStrings[j].length() > i)
+          sb.append(analysisStrings[j].charAt(i));
+      }
+
+      // Do frequency counting over characters in same position over all
+      // analysed ciphers
+      FrequencyCounter fc = new FrequencyCounter();
+      fc.count(sb.toString());
+
+      // In the case that there are no characters recorded in this column,
+      // unable to determine the plain text character
+      if (fc.totalCount() == 0)
+      {
+        messageChars[i] = '-';
+        continue;
+      }
+
+      double[] norm = fc.normalise();
+
+      // Find most frequent character in count
+      int normMax = 0;
+      for (int j = 1; j < norm.length; j++)
+      {
+        if (norm[j] > norm[normMax])
+          normMax = j;
+      }
+
+      // If the majority is all one character then the plain text message is
+      // likely to be that character
+      if (norm[normMax] >= validCharThreshold)
+        messageChars[i] = Utils.getCharFromIndex(normMax);
+      // Otherwise it is likely to be a space
+      else
+        messageChars[i] = ' ';
+    }
+
+    return new String(messageChars);
+  }
 
   private byte[][] m_ciphers;
 }
--- coursework1_implementation/src/test/java/com/dan_nixon/csc3621/cw1/ex3/OTPAttackTest.java
+++ coursework1_implementation/src/test/java/com/dan_nixon/csc3621/cw1/ex3/OTPAttackTest.java
@@ -2,9 +2,76 @@ package com.dan_nixon.csc3621.cw1.ex3;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.io.File;
+import java.io.IOException;
+import com.dan_nixon.csc3621.cw1.Utils;
 import com.dan_nixon.csc3621.cw1.ex3.OTPAttack;
 
 public class OTPAttackTest
 {
-  //TODO
+  @Test
+  public void testLoadCiphersFromFile() throws IOException
+  {
+    File c1 = new File(this.getClass().getResource("/ex3_cw_cipher_1.bin").getFile());
+    File c2 = new File(this.getClass().getResource("/ex3_cw_cipher_2.bin").getFile());
+    File c3 = new File(this.getClass().getResource("/ex3_cw_cipher_3.bin").getFile());
+    File c4 = new File(this.getClass().getResource("/ex3_cw_cipher_4.bin").getFile());
+    File c5 = new File(this.getClass().getResource("/ex3_cw_cipher_5.bin").getFile());
+    File c6 = new File(this.getClass().getResource("/ex3_cw_cipher_6.bin").getFile());
+    File c7 = new File(this.getClass().getResource("/ex3_cw_cipher_7.bin").getFile());
+
+    File[] cipherFiles = {c1, c2, c3, c4, c5, c6, c7};
+    byte[][] ciphers = OTPAttack.loadCiphersFromFiles(cipherFiles);
+
+    // Test number of ciphers loaded
+    assertEquals(7, ciphers.length);
+
+    // Test some values
+    assertEquals((byte) 0xDC, ciphers[0][0]);
+    assertEquals((byte) 0xF7, ciphers[6][5]);
+  }
+
+  @Test
+  public void testGetAnalysisStrings() throws IOException
+  {
+    File c1 = new File(this.getClass().getResource("/ex3_cw_cipher_1.bin").getFile());
+    File c2 = new File(this.getClass().getResource("/ex3_cw_cipher_2.bin").getFile());
+    File c3 = new File(this.getClass().getResource("/ex3_cw_cipher_3.bin").getFile());
+    File c4 = new File(this.getClass().getResource("/ex3_cw_cipher_4.bin").getFile());
+    File c5 = new File(this.getClass().getResource("/ex3_cw_cipher_5.bin").getFile());
+    File c6 = new File(this.getClass().getResource("/ex3_cw_cipher_6.bin").getFile());
+    File c7 = new File(this.getClass().getResource("/ex3_cw_cipher_7.bin").getFile());
+
+    File[] cipherFiles = {c1, c2, c3, c4, c5, c6, c7};
+    OTPAttack otpa = new OTPAttack(cipherFiles);
+
+    File target = new File(this.getClass().getResource("/ex3_cw_cipher_target.bin").getFile());
+    byte[] targetCipher = Utils.readBinaryFile(target);
+
+    String[] analysisStrings = otpa.getAnalysisStrings(targetCipher);
+    assertEquals(7, analysisStrings.length);
+  }
+
+  @Test
+  public void testGuessPlainText() throws IOException
+  {
+    File c1 = new File(this.getClass().getResource("/ex3_cw_cipher_1.bin").getFile());
+    File c2 = new File(this.getClass().getResource("/ex3_cw_cipher_2.bin").getFile());
+    File c3 = new File(this.getClass().getResource("/ex3_cw_cipher_3.bin").getFile());
+    File c4 = new File(this.getClass().getResource("/ex3_cw_cipher_4.bin").getFile());
+    File c5 = new File(this.getClass().getResource("/ex3_cw_cipher_5.bin").getFile());
+    File c6 = new File(this.getClass().getResource("/ex3_cw_cipher_6.bin").getFile());
+    File c7 = new File(this.getClass().getResource("/ex3_cw_cipher_7.bin").getFile());
+
+    File[] cipherFiles = {c1, c2, c3, c4, c5, c6, c7};
+    OTPAttack otpa = new OTPAttack(cipherFiles);
+
+    File target = new File(this.getClass().getResource("/ex3_cw_cipher_target.bin").getFile());
+    byte[] targetCipher = Utils.readBinaryFile(target);
+
+    String[] analysisStrings = otpa.getAnalysisStrings(targetCipher);
+    String messageGuess = otpa.guessPlainTextFromAnalysis(analysisStrings, targetCipher.length, 0.8);
+
+    assertEquals("-ou hav- d-n- your w-rk", messageGuess);
+  }
 }
